9e973c61d4221ed90dab3f1edb99645c5ab1fef4
helich0pper <helich0pper@email.com>
2020-11-17 09:08:45 +0200

    Added favicon

--- bin/Server/app.py
+++ bin/Server/app.py
@@ -80,5 +80,5 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run("0.0.0.0", PORT)
     
