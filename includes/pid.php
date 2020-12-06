24c1eeb2c32ca6fa6068e6f0bcfb22d41147a808
helich0pper <59028550+helich0pper@users.noreply.github.com>
2020-12-06 22:45:33 +0200

    Update pid.php

--- includes/pid.php
+++ includes/pid.php
@@ -3,10 +3,10 @@ $pid = null;
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-        // Change this for Windows
+        // Change "python" for Windows
         $pid = exec("start python ../bin/Server/app.py & ");
     } else {
-        // Change this for Linux/BSD
+        // Change "python3" for Linux/BSD
         $pid = exec("python3 ../bin/Server/app.py &");
     }
 
